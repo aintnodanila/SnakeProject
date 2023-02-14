852db2782eea3adf74cdfaf2eab4b948cfa3cae5
Danila Rusak <skinny4550@gmail.com>
2023-02-14 13:36:47 +0100

    Add new methods, declare variables

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -2,9 +2,53 @@
 {
     internal class Program
     {
+        bool quit = false;
+        int vx;
+        int vy;
+        int headX;
+        int headY;
+        int[,] GameField;
+        int w = 80, h = 40;
+        int score = 0;
+        int lifes = 3;
+        int apples;
+        int level = 0;
+
+
+        void SplashScreen()
+        {
+            string[] ss = new string[10];
+
+            ss[0] = "  %%%   %    %   %%% %   %   %%%%%%";
+            ss[1] = " %   %  %    %  %  % %  %    %     ";
+            ss[2] = "  %     %%   % %   % % %     %     ";
+            ss[3] = "   %    % %  % %%%%% %%      %%%%% ";
+            ss[4] = "    %   %  % % %   % % %     %     ";
+            ss[5] = "     %  %   %% %   % %  %    %     ";
+            ss[6] = " %   %  %    % %   % %   %   %     ";
+            ss[7] = "  %%%   %    % %   % %    %  %%%%%%";
+            ss[8] = "                                   ";
+            ss[9] = " - USE THE ARROW BUTTONS TO MOVE - ";
+
+            for (int i = 0; i < ss.Length; i++)
+                for (int j = 0; j < ss[i].Length; j++)
+                {
+                    Console.SetCursorPosition(j + 25, i + 10);
+                    Console.Write(ss[i][j]);
+                    System.Threading.Thread.Sleep(5);
+                }
+            Console.SetCursorPosition(30, 25);
+            Console.Write("Press any key to start");
+            Console.ResetColor();
+        }
+        public void Game()
+        {
+            SplashScreen();
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("snake");
+            Program program = new Program();
+            program.Game();
         }
     }
 }
